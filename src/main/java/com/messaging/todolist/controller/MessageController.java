494a0639a07e162ae14d5cf83011d5836e2423eb
Amandeep Singh Chhatwal <aman21singh@gmail.com>
2023-07-27 00:07:23 +0530

    Create MessageController.java
    Message controller

---
+++ src/main/java/com/messaging/todolist/controller/MessageController.java
@@ -0,0 +1,35 @@
+package com.messaging.todolist.controller;
+
+
+import com.messaging.todolist.service.MessageService;
+import com.messaging.todolist.util.Utils;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/v1")
+@AllArgsConstructor
+public class MessageController {
+
+    private static Logger logger = LoggerFactory.getLogger(MessageController.class);
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/sendmessage")
+    public void getMessage(@RequestBody String body) {
+        logger.info("MessageController json body" + body);
+        Map<String, Object> requestBody = Utils.convertStringToMap(body);
+        String message = String.valueOf(requestBody.get("message"));
+        logger.info("message" + message);
+        messageService.sendReply(message);
+    }
+
+}
